4e664b00b51d597f1a297271d440a8815ccaf73d
dprvulov <dejan.prv@gmail.com>
2019-12-24 17:26:45 +0100

    first commit

---
+++ src/Components/Header.svelte
@@ -0,0 +1,15 @@
+<nav>
+  <ul>
+    <li>Home</li>
+    <li>About</li>
+  </ul>
+</nav>
+
+<style lang="scss">
+  nav {
+    max-width: 80vw;
+    margin: 0 auto;
+    width: 100%;
+    border: 1px solid black;
+  }
+</style>
