04120229e1ca16ecc66be6f41c36cd728b860d8a
dprvulov <dejan.prv@gmail.com>
2019-12-25 17:41:42 +0100

    svg

--- src/Components/Index.svelte
+++ src/Components/Index.svelte
@@ -11,7 +11,8 @@
 </script>
 
 
-<div class="center">
+<div class="center-row">
+  <img src="./Assets/analysis.svg" alt="Excel Illustration">
   <div class="white-box">
     <h1>Excel Formeln verstehen</h1>
     <textarea bind:value={input}></textarea>
@@ -22,20 +23,38 @@
 <style lang="scss">
   @import "./styles/colors";
   
-  .center {
+  .center-row {
     min-height: 100vh;
   }
 
   .white-box {
     background: $white;
-    width: 70%;
+    width: 60%;
     padding: 1rem;
     border-radius: 10px;
     border: 3px solid $blue-200;
+    top: 25%;
+    position: absolute;
+  }
+
+
+  @media (max-width: 900px) {
+    .white-box {
+      width: 75%;
+    }
   }
 
   pre {
     border-radius: 5px;
+  }
+
+  img {
+    max-width: 200px;
+    max-height: 200px;
+    top: 10%;
+    position: fixed;
+    margin: 0 auto;
+    z-index: 1;
   }
 
   
