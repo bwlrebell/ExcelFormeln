2834b1b4e2c51c0f9eeb8a980cee8f6670ca5e35
dprvulov <dejan.prv@gmail.com>
2020-01-05 21:10:09 +0100

    language switch

--- src/Helpers/formula-dev.js
+++ src/Helpers/formula-dev.js
@@ -3,9 +3,10 @@
 /**
  * Take a row formula string and return it formatted
  * @param {string} input - Input formula string
+ * @param {string} lang - Language setting
  * @return {string} output a formatted formula string
  */
-function formatFormula(input) {
+function formatFormula(input, lang = "en") {
   // ReplaceAt Function
   let replaceAt = function(string, index, replacement) {
     const left = string.substr(0, index);
@@ -13,59 +14,77 @@ function formatFormula(input) {
     return left + replacement + right;
   }
 
+  try {
+    // Check if input is undefined
+    input = (input !== undefined) ? String(input) : "";
+    
+    // Check if empty
+    if (input.length === 0) {
+      return "Empty String";
+    }
 
-  // Check if input is undefined
-  input = (input !== undefined) ? String(input) : "";
-  
-  // Check if empty
-  if (input.length === 0) {
-    return "Empty String";
+    // Check if formula starts with "="
+    if(input[0] !== "=") {
+      input = "=" + input;
+    }
+
+
+
+    // Formatting
+    if (lang === "de") {
+      input = input.replace(/\;\s/g, ";");
+    } else {
+      input = input.replace(/\;\s/g, ",");
+    }
+    
+    let deep = 0;
+    for (let i = 0; i < input.length; i++) {
+      let chr = input[i];
+      let delta = input.length;
+
+      if(chr === "=") {
+        input = replaceAt(input, i, "=");
+        delta = input.length - delta;
+        i = i + delta;
+      }
+
+      if(chr === "(") {
+        deep += 1;
+        input = replaceAt(input, i, " ( \n" + "\t".repeat(deep));
+        delta = input.length - delta;
+        i = i + delta;
+      }
+
+      if (lang === "de") {
+        if(chr === ";") {
+          input = replaceAt(input, i, ";\n" + "\t".repeat(deep));
+          delta = input.length - delta;
+          i = i + delta;
+        }
+      } else {
+          if(chr === ",") {
+            input = replaceAt(input, i, ",\n" + "\t".repeat(deep));
+            delta = input.length - delta;
+            i = i + delta;
+          }
+      }
+      
+      if(chr === ")") {
+        deep -= 1;
+        input = replaceAt(input, i, "\n" + "\t".repeat(deep) + ")");
+        delta = input.length - delta;
+        i = i + delta;
+      }
+    }
+
+    input = input.replace(/\t/g, " ".repeat(4));
+    input = input.trim()
+
+    let result = input;
+    return result
+  } catch(err) {
+    return err.message
   }
-
-  // Check if formula starts with "="
-  if(input[0] !== "=") {
-    input = "=" + input;
-  }
-
-
-  // String Replacement
-  input = input.replace(/\;\s/g, ";");
-  let deep = 0;
-  for (let i = 0; i < input.length; i++) {
-    let chr = input[i];
-    let delta = input.length;
-
-    if(chr === "=") {
-      input = replaceAt(input, i, " = ");
-      delta = input.length - delta;
-      i = i + delta;
-    }
-
-    if(chr === "(") {
-      deep += 1;
-      input = replaceAt(input, i, " ( \n" + "\t".repeat(deep));
-      delta = input.length - delta;
-      i = i + delta;
-    }
-
-    if(chr === ";") {
-      input = replaceAt(input, i, ";\n" + "\t".repeat(deep));
-      delta = input.length - delta;
-      i = i + delta;
-    }
-
-    if(chr === ")") {
-      deep -= 1;
-      input = replaceAt(input, i, "\n" + "\t".repeat(deep) + ")");
-      delta = input.length - delta;
-      i = i + delta;
-    }
-  }
-
-  input = input.trim()
-
-  let result = input;
-  return result
 }
 
 // console.log(formatFormula());
@@ -76,5 +95,4 @@ function formatFormula(input) {
 // console.log(formatFormula("=Something<script>alert('x //')</script>"));
 console.log(formatFormula("=SVERWEIS(x;x;x)"))
 console.log(formatFormula('=SVERWEIS(Wenn(y=3;y;y); x; x)'));
-console.log(formatFormula('=SVERWEIS(Wenn(y<3;y;y); x; x)'));
-console.log(formatFormula('=SVERWEIS(Wenn(y<3;"YES";"NO"); OTHERFUNCTION(3,14; 3; SVERWEIS(X;Y)); x)'));
+console.log(formatFormula('=SVERWEIS(Wenn(y<3;"YES";"NO"); OTHERFUNCTION(3,14; 3; SVERWEIS(X;Y)); x)', "de"));
