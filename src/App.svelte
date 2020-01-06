67840e2a8b5079adf64957913aeac3d833ad1184
dprvulov <dejan.prv@gmail.com>
2020-01-06 20:13:23 +0100

    Rename Index to Prettifier

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
 import Router from "svelte-spa-router";
 import GlobalStyle from "./Components/GlobalStyle.svelte";
-import Index from "./Components/Index.svelte";
+import Prettifier from "./Components/Prettifier.svelte";
 import Page from "./Components/Page.svelte";
 
 const routes = {
-  "/" : Index,
+  "/" : Prettifier,
   "/page" : Page
 };
 
